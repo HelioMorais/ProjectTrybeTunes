21dfa4ca62a931ab0e22a6465bfc3aa96a071706
Helio Morais <heliogov20@gmail.com>
2023-04-13 15:53:33 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './components/Content';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTuuuunes</p>);
+    return (
+      <BrowserRouter>
+        <section>
+          <Content />
+        </section>
+      </BrowserRouter>
+    );
   }
 }
 
