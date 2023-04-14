6280c41ba762b7e06e1d04b39cb65fe96d15e52c
Helio Morais <heliogov20@gmail.com>
2023-04-13 22:17:59 -0300

    Requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Loading from './Loading';
+import Music from '../components/Music';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    album: [],
+  };
+
+  componentDidMount() {
+    const { match: { params } } = this.props;
+    getMusics(params.id).then((result) => this.setState({
+      album: result,
+    }));
+  }
+
+  renderAlbumPage = () => {
+    const { album } = this.state;
+    return (
+      <div data-testid="page-album">
+        <span data-testid="artist-name">
+          Artista/Banda:
+          {' '}
+          {album[0].artistName}
+        </span>
+        {' '}
+        <br />
+        <span data-testid="album-name">
+          Coleção:
+          {' '}
+          {album[0].collectionCensoredName}
+        </span>
+      </div>
+    );
+  };
+
   render() {
+    const { album } = this.state;
     return (
       <div>
         <Header />
-        <div data-testid="page-album">Album</div>
+        {(album.length === 0)
+          ? <Loading /> : this.renderAlbumPage()}
+        <Music album={ album } />
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
