6c5498805601c39500921faf5dc604d5f6b1551d
Helio Morais <heliogov20@gmail.com>
2023-04-13 21:42:21 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     buttonEnable: true,
+    name: '',
+    albums: [],
+    label: false,
+  };
+
+  getAlbums = () => {
+    const { name } = this.state;
+    const artist = name;
+    this.setState({
+      saveName: name,
+      name: '',
+      label: true,
+    }, () => searchAlbumsAPI(artist)
+      .then((result) => this.setState({
+        albums: result,
+        label: false,
+      })));
+  };
+
+  messageAlbum = () => {
+    const { saveName } = this.state;
+    return (
+      <span>
+        Resultado de álbuns de:
+        {' '}
+        {saveName}
+      </span>
+    );
   };
 
   handleChange = ({ target }) => {
@@ -27,27 +58,62 @@ class Search extends Component {
     }
   };
 
+  returnLabel = () => {
+    const {
+
+      buttonEnable,
+      name,
+      albums,
+      saveName,
+
+    } = this.state;
+    return (
+      <label htmlFor="name">
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="name"
+          onChange={ this.handleChange }
+          value={ name }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          onClick={ this.getAlbums }
+          disabled={ buttonEnable }
+        >
+          Pesquisar
+        </button>
+        {(albums.length !== 0)
+          && this.messageAlbum()}
+        {(albums.length === 0 && saveName !== undefined)
+          && <span>Nenhum álbum foi encontrado</span>}
+      </label>
+    );
+  };
+
   render() {
-    const { buttonEnable } = this.state;
+    const { albums, label } = this.state;
     return (
       <div>
         <Header />
         <div data-testid="page-search">Search</div>
-        <label htmlFor="name">
-          <input
-            data-testid="search-artist-input"
-            type="text"
-            name="name"
-            onChange={ this.handleChange }
-          />
-          <button
-            data-testid="search-artist-button"
-            type="submit"
-            disabled={ buttonEnable }
-          >
-            Pesquisar
-          </button>
-        </label>
+        {label
+          ? <Loading /> : this.returnLabel() }
+        <section>
+          {albums.map((element) => (
+            <div key={ element.collectionId }>
+              <Link
+                data-testid={ `link-to-album-${element.collectionId}` }
+                to={ `/album/${element.collectionId}` }
+              >
+                <div>{element.artistName}</div>
+                <img src={ element.artworkUrl100 } alt={ element.collectionName } />
+                <div>{element.collectionName}</div>
+              </Link>
+            </div>
+          ))}
+        </section>
       </div>
     );
   }
